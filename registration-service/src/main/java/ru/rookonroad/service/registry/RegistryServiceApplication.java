d56c6f2692abfd910970d3f02b839efb28b36e73
Ilya <ilia992@gmail.com>
2021-03-10 09:19:40 +0300

    Initial commit

---
+++ registration-service/src/main/java/ru/rookonroad/service/registry/RegistryServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.rookonroad.service.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryServiceApplication.class, args);
+    }
+}
